d5ed87568ae81ac5478e37491f09b8a05b2eb02d
oomi <whdudal1217@naver.com>
2022-12-09 16:15:33 +0900

    :sparkles: 인증 엔터티 추가

---
+++ src/main/java/jwt/security/study/entity/Authority.java
@@ -0,0 +1,22 @@
+package jwt.security.study.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "authority")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+
+}
