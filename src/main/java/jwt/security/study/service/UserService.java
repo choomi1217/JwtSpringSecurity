b3d4e4475e387ce23c7bde6a21c47de88e4b34b2
oomi <whdudal1217@naver.com>
2022-12-09 18:00:19 +0900

    :sparkles: 유저서비스 추가

---
+++ src/main/java/jwt/security/study/service/UserService.java
@@ -0,0 +1,17 @@
+package jwt.security.study.service;
+
+import jwt.security.study.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+}
