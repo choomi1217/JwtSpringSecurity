47d8d4ef2dacdf42c683d3371110ad1f5ec2e886
choomi <whdudal1217@naver.com>
2022-12-12 22:33:54 +0900

    :sparkles: interface inner class 속 record였던 SignupRequest 분리

--- src/main/java/jwt/security/study/user/repository/UserRepository.java
+++ src/main/java/jwt/security/study/user/repository/UserRepository.java
@@ -1,49 +1,37 @@
 package jwt.security.study.user.repository;
 
+import jwt.security.study.user.SignupRequest;
+import jwt.security.study.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import static jwt.security.study.user.repository.User.init;
+import static jwt.security.study.user.entity.User.init;
 
 @FunctionalInterface
 public interface UserRepository {
-    User signUp(UserRdbmsRepository.SignupRequest request);
+    User signUp(SignupRequest request);
 
 
-    static UserRdbmsRepository.SignupRequest format(String username, String nickname, String password){
-        return new UserRdbmsRepository.SignupRequest(username,nickname,password);
+    static SignupRequest format(String username, String nickname, String password){
+        return new SignupRequest(username,nickname,password);
     }
 
     @Service
     @RequiredArgsConstructor
     class UserRdbmsRepository implements UserRepository{
-        private final UserJpaRepository userRepository;
+        private final UserJpaRepository userJpaRepository;
         private final PasswordEncoder passwordEncoder;
 
         @Override
         public User signUp(SignupRequest request){
-            if( userRepository.existsByUsername(request.username)){
+            if( userJpaRepository.existsByUsername(request.username())){
                 throw new IllegalArgumentException("이미 가입되어 있는 유저입니다.");
             }
-            return userRepository.save(init(request.username,request.nickname,request.encrypt(passwordEncoder)));
-        }
-        public record SignupRequest(String username, String nickname, String password){
-            public SignupRequest {
-                if (username == null || username.isBlank()) {
-                    throw new IllegalArgumentException("username은 필수값입니다.");
-                }
-                if (nickname == null || nickname.isBlank()) {
-                    throw new IllegalArgumentException("nickname은 필수값입니다.");
-                }
-                if (password == null || password.isBlank()) {
-                    throw new IllegalArgumentException("password는 필수값입니다.");
-                }
-            }
 
-            private String encrypt(PasswordEncoder passwordEncoder){
-                return passwordEncoder.encode(password);
-            }
+            User user = init(request.username(),request.nickname(), request.encrypt(passwordEncoder));
+
+            return userJpaRepository.save(user);
         }
     }
 
