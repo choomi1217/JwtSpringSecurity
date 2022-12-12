4bd8152a2c7e92cc2d654cff829974e966a1659d
choomi <whdudal1217@naver.com>
2022-12-12 23:00:14 +0900

    refactor: change import
    - change some imports

--- src/main/java/jwt/security/study/user/repository/UserJpaRepository.java
+++ src/main/java/jwt/security/study/user/repository/UserJpaRepository.java
@@ -1,10 +1,11 @@
 package jwt.security.study.user.repository;
 
+import jwt.security.study.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-interface UserJpaRepository extends JpaRepository<User, Long> {
+public interface UserJpaRepository extends JpaRepository<User, Long> {
     Optional<User> findOneWithAuthoritiesByUsername(String username);
 
     boolean existsByUsername(String username);
